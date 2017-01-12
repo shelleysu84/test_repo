61190f9a0e781143e22194069526bdd39a2608cc
shelleysu84 <shelleysu84@gmail.com>
2017-01-11 16:54:04 -0800

    submit output results

---
+++ api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AppClass",
+        "StubbedController"
+    ],
+    "modules": [
+        "js_microservice_skeleton"
+    ],
+    "allModules": [
+        {
+            "displayName": "js_microservice_skeleton",
+            "name": "js_microservice_skeleton"
+        }
+    ],
+    "elements": []
+} };
+});
